0c958c39eea78ce1e828478c3d9a9adc522af52f
Manisha Ghadei <manisha.ghadei91981@gmail.com>
2025-12-15 22:26:45 +0530

    check for existing users

--- register.js
+++ register.js
@@ -3,21 +3,34 @@ const form = document.getElementById("registerForm");
 form.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    fetch("http://localhost:3000/users", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            username: document.getElementById("username").value,
-            password: document.getElementById("pswd").value
+    const userName = document.getElementById("username").value;
+    const pass = document.getElementById("pswd").value;
+
+    fetch("http://localhost:3000/users")
+        .then(res => res.json())
+        .then(users => {
+            const existUser = users.find(u => u.username === userName);
+
+            if (existUser) {
+                alert("User already exists. Please login.");
+                window.location.href = "login.html";
+            }
+
+            fetch("http://localhost:3000/users", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    username: userName,
+                    password: pass
+                })
+            });
         })
-    })
-    .then(function () {
-        alert("Registration successful");
-        window.location.href = "/login.html";
-    })
-    .catch(function () {
-        alert("Something went wrong");
-    });
+        .then(() => {
+            alert("Registration successful");
+            window.location.href = "../login.html";
+            }
+        )
+        .catch(() => {
+            alert("Error 404: Something went wrong");
+        });
 });
